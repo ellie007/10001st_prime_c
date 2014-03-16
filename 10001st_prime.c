a7581a909007d29fb4287cd9a6c472f32f02bccc
Eleanor Lee <ellie007@gmail.com>
2014-03-16 17:29:15 -0500

    solution

---
+++ 10001st_prime.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int is_prime(num){
+  int i;
+  int result = false;
+
+  for(i = 2; i <= num - 1; i++){
+    if (num % i == 0){
+      result = false;
+      return result;
+    }
+  }
+  result = true;
+  return result;
+}
+
+int ten001st_prime(){
+  int number;
+  int counter = 0;
+  int upper_bound = 10001;
+
+  for (number = 2; counter < upper_bound; ++number){
+    if(is_prime(number)){
+      counter += 1;
+        if (counter == upper_bound){break;}
+    }
+  }
+  printf("%i\n", number);
+  return number;
+}
+
+int main(){
+  return ten001st_prime();
+}
